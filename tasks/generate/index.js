2e147d5d161337dbb9d6d42dbcf245f0de04e18b
Quentin Neyraud <quentin.neyraud@gmail.com>
2019-12-04 14:57:02 +0100

    Update templates

--- tasks/generate/index.js
+++ tasks/generate/index.js
@@ -2,16 +2,12 @@ const getUserInput = require('./userInput')
 const moveFiles = require('./files')
 const { logSuccessMessage } = require('./log')
 
-module.exports = () => {
-  logSuccessMessage({
-    packageName: 'my-awesome-library'
-  })
-  // getUserInput()
-  //   .then(moveFiles)
-  //   .then(d => {
-  //     console.log('OK')
-  //   })
-  //   .catch(err => {
-  //     console.log('Error', err)
-  //   })
+module.exports = async () => {
+  const userInput = await getUserInput()
+
+  moveFiles(userInput)
+    .then(logSuccessMessage.bind(null, userInput))
+    .catch(err => {
+      console.log('Error', err)
+    })
 }
